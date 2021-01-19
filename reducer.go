27422e8b5f78dbab75f15ff8d54f798010ae88e3
Ryan Maxwell Rasi <theryanrasi@gmail.com>
2021-01-19 19:07:12 +0000

    Reducer now correctly seperates extra data

--- reducer.go
+++ reducer.go
@@ -9,46 +9,19 @@ import (
 	"strings"
 )
 
-func reducer(input1 map[string]int, input2 map[string]int, input3 map[string]int, input4 map[string]int, input5 map[string]int, input6 map[string]int, input7 map[string]int, input8 map[string]int) {
+func reducer(inputData map[int]map[string]int, numberOfCPUs int) {
+	//PASSENGERS ON EACH FLIGHT
 	dictPassengersOnEachFlight := make(map[string]int)
-
-	for key, value := range input1 {
-		dictPassengersOnEachFlight[key] = dictPassengersOnEachFlight[key] + value
-		//Probs if statement
-	}
-	for key, value := range input2 {
-		dictPassengersOnEachFlight[key] = dictPassengersOnEachFlight[key] + value
-		//Probs if statement
-	}
-	for key, value := range input3 {
-		dictPassengersOnEachFlight[key] = dictPassengersOnEachFlight[key] + value
-		//Probs if statement
-	}
-	for key, value := range input4 {
-		dictPassengersOnEachFlight[key] = dictPassengersOnEachFlight[key] + value
-		//Probs if statement
-	}
-	for key, value := range input5 {
-		dictPassengersOnEachFlight[key] = dictPassengersOnEachFlight[key] + value
-		//Probs if statement
-	}
-	for key, value := range input6 {
-		dictPassengersOnEachFlight[key] = dictPassengersOnEachFlight[key] + value
-		//Probs if statement
-	}
-	for key, value := range input7 {
-		dictPassengersOnEachFlight[key] = dictPassengersOnEachFlight[key] + value
-		//Probs if statement
-	}
-	for key, value := range input8 {
-		dictPassengersOnEachFlight[key] = dictPassengersOnEachFlight[key] + value
-		//Probs if statement
+	for i := 0; i < numberOfCPUs; i++ {
+		for key, value := range inputData[i] {
+			dictPassengersOnEachFlight[key] = dictPassengersOnEachFlight[key] + value
+		}
 	}
 	outputArray := ""
 	for key, value := range dictPassengersOnEachFlight {
 		if key == "--" {
 		} else {
-			outputArray = outputArray + strings.Replace(key, "-", "    ", -1) + "    " + fmt.Sprint(value) + "\n"
+			outputArray = outputArray + strings.Replace(key, "-", "        ", -1) + "        " + fmt.Sprint(value) + "\n"
 		}
 	}
 	//fmt.Println(outputArray)
